5a9e89fe264570d5e9757e6832b204778a022c6a
Rafael Caixeta <caixetadev@gmail.com>
2021-12-31 18:11:58 -0300

    adicionando o dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import Mongoose from "mongoose";
+import "dotenv/config";
+
+const USER_DB = process.env.USER_DB;
+const PASSWORD_DB = process.env.PASSWORD_DB;
 
 const app = express();
 
@@ -9,5 +14,19 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send({ message: "Hello World" });
 });
+
+const connectDB = (): void => {
+  try {
+    Mongoose.connect(
+      `mongodb+srv://${USER_DB}:${PASSWORD_DB}@apiportfolio.dq7se.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    );
+
+    console.log("Mongoose conected");
+  } catch (error) {
+    console.log({ error: error });
+  }
+};
+
+connectDB();
 
 export { app };
