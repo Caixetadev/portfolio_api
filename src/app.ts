41848c6290be95861a307cd135590ddb96309157
Rafael Caixeta <caixetadev@gmail.com>
2021-12-31 22:33:54 -0300

    configurando cors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import Mongoose from "mongoose";
 import "dotenv/config";
-import { router } from "./routes/routes";
+import { options, router } from "./routes/routes";
 
 const USER_DB = process.env.USER_DB;
 const PASSWORD_DB = process.env.PASSWORD_DB;
@@ -10,7 +10,7 @@ const PASSWORD_DB = process.env.PASSWORD_DB;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(options));
 app.use(router);
 
 const connectDB = (): void => {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,11 +5,18 @@ import {
   HomePost,
   HomeUpdate,
 } from "../controllers/homeController";
+import cors from "cors";
 const router = Router();
+
+const whitelist = ["https://gbxxy.com/"];
+
+const options: cors.CorsOptions = {
+  origin: whitelist,
+};
 
 router.get("/", Home);
 router.post("/", HomePost);
 router.delete("/:id", HomeDelete);
 router.patch("/:id", HomeUpdate);
 
-export { router };
+export { router, options };
